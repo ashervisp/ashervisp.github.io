8e9a3e076b50a8c61913eee231b77e17c76af1f0
ashervisp <ashvisperas@gmail.com>
2021-03-17 17:14:07 -0500

    2021 Change Portfolio

--- README.md
+++
@@ -1,2 +0,0 @@
-# My Portfolio Site!
-This is the code for my portfolio site where I display my projects so far and info about me. It is being updated frequently, so right now is a work in progress! :)
--- script.js
+++ script.js
@@ -1,16 +1,89 @@
+let i = 0;
+let j = 0;
+let text = 'Asher Visperas'
+let text2 = 'ashvisperas@gmail.com'
+let speed = 1000/8;
 
 
-function title_change(){
-  let title = [' Code',' Server',' Script', ' Program', ' Scrap']
-  const random = Math.floor(Math.random() * title.length);
-  document.getElementById('title_change').innerHTML = title[random];
+function typingEffect(){
+  if(i < text.length ){
+    document.querySelector('#title').innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typingEffect,speed);
+  }
+
 }
 
-function open_text(){
-  var x = document.getElementByByClass("links");
+function showJavascript(){
+  document.querySelector('#webDev').classList.remove('animatee');
+  document.querySelector('#webDev').classList.remove('animatee2');
+  document.querySelector('#webDev').classList.remove('animatee3');
+  document.querySelectorAll('.firstProject .secondProject .thirdProject').innerHTML = '';
+  setTimeout(function(){
+    document.querySelector('#webDev').classList.add('animatee');
+    document.querySelector('#portfolio').classList.add('animatee');
+  },300);
+  document.querySelector('.firstProject').innerHTML = 'Browser Game';
+  document.querySelector('.secondProject').innerHTML = 'Calendar';
+  document.querySelector('.thirdProject').innerHTML = 'Boba Tea';
+  document.querySelector('#one').src = 'bootaPlayer.png';
+  document.querySelector('#two').src = 'Calendar.png';
+  document.querySelector('#three').src = 'boba.jpg';
 
-}
+  document.querySelector('#portfolio').style.backgroundColor = '#355C7D';
+
+  document.querySelector('#oneLink').href = 'https://github.com/ashervisp/discordBot'
+  document.querySelector('#twoLink').href = 'https://github.com/ashervisp/Developer-Portfolio/tree/master/schedule_project'
+  document.querySelector('#threeLink').href = 'https://github.com/ashervisp/Boba_Shop'
+
+};
+
+function showData(){
+  document.querySelector('#portfolio').classList.remove('animatee');
+    document.querySelector('#webDev').classList.remove('animatee');
+    document.querySelector('#webDev').classList.remove('animatee3');
+    document.querySelectorAll('.firstProject .secondProject .thirdProject').innerHTML = '';
+  setTimeout(function(){
+    document.querySelector('#webDev').classList.add('animatee2');
+    document.querySelector('#portfolio').classList.add('animatee');
+  },300);
+  document.querySelector('.firstProject').innerHTML = 'Housing Data';
+  document.querySelector('.secondProject').innerHTML = 'Netflix Data';
+  document.querySelector('.thirdProject').innerHTML = '';
+  document.querySelector('#one').src = 'home.png';
+  document.querySelector('#two').src = 'netflix.png';
+  document.querySelector('#three').src = '';
+
+  document.querySelector('#portfolio').style.backgroundColor = '#355C7D';
+
+  document.querySelector('#oneLink').href = 'https://github.com/ashervisp/California_Housing1990'
+  document.querySelector('#twoLink').href =  'https://github.com/ashervisp/Netflix-Analysis'
+  document.querySelector('#threeLink').href = ''
+
+};
+
+function showExtra(){
+  document.querySelector('#portfolio').classList.remove('animatee');
+  document.querySelector('#webDev').classList.remove('animatee');
+  document.querySelector('#webDev').classList.remove('animatee2');
+  document.querySelectorAll('.firstProject .secondProject .thirdProject').innerHTML = '';
+setTimeout(function(){
+  document.querySelector('#webDev').classList.add('animatee3');
+  document.querySelector('#portfolio').classList.add('animatee');
+},300);
+  document.querySelector('.firstProject').innerHTML = 'Discord Bot';
+  document.querySelector('.secondProject').innerHTML = '';
+  document.querySelector('.thirdProject').innerHTML = '';
+  document.querySelector('#one').src = 'discordicon.png';
+  document.querySelector('#two').src = '';
+  document.querySelector('#three').src = '';
+
+  document.querySelector('#oneLink').href = 'https://github.com/ashervisp/discordBot'
+  document.querySelector('#twoLink').href = ''
+  document.querySelector('#twoLink').href = ''
+
+  document.querySelector('#portfolio').style.backgroundColor = '#355C7D';
+};
 
 
-
-setInterval(title_change,3000);
+typingEffect();
